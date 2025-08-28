8de58640f9e7f2afa10e3f15b1232ecf8e9b1c35
Pedro Lunet <pedrolunet@significa.co>
2025-08-28 15:51:30 +0100

    fixed lint

--- src/mail.service.ts
+++ src/mail.service.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
 import * as nodemailerImport from 'nodemailer';
 type Transporter = typeof nodemailerImport.createTransport extends (
